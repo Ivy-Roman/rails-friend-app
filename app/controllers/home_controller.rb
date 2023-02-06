d86a636bb3e66adeb8baca8275a13cc3b5095bda
Ivie Osoiye <ivieosoiye@gmail.com>
2023-02-06 12:20:00 +0100

    edited the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   end
 
   def about
+    @about_me = "Friends App is a user friendly app designed to help you connect with friends. It is the first Rails project by Ivie Osoiye for the purpose of learning. It may not look pretty, but she assures you, its functional *wink*. Have fun! See you on the other side. "
   end
 end
